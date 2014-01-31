fb5c4f68055b51eb9a27546e88fe0099e89e1159
The Zelus <thezelus@gmail.com>
2014-01-30 20:40:29 -0700

    updating test

--- src/test/java/AnagramDetectionTest.java
+++ src/test/java/AnagramDetectionTest.java
@@ -3,6 +3,8 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class AnagramDetectionTest {
     @Test
     public void testHeapPermute() throws Exception {
@@ -10,6 +12,9 @@ public class AnagramDetectionTest {
         String testString = "ABCD";
         ArrayList<String> testAnagramList = Lists.newArrayList();
         anagramDetection.heapPermute(testString.length(), testString.toCharArray(), testAnagramList);
-        assert(testAnagramList.size() != 0);
+        assert(testAnagramList.size() == 24);
+        assert(testAnagramList.contains("ABCD"));
+        assert(testAnagramList.contains("DCBA"));
+        assert(testAnagramList.contains("CDAB"));
     }
 }
